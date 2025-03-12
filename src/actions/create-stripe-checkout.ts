6f49ee099957a21daae745d5cab02dac339e9e93
Joao Victor <devjoaovictor10@gmail.com>
2025-03-12 20:52:22 -0300

    fix: remover try createStripeCheckout

--- src/actions/create-stripe-checkout.ts
+++ src/actions/create-stripe-checkout.ts
@@ -25,43 +25,39 @@ export const createStripeCheckout = async ({
   products,
   consumptionMethod,
 }: CreateStripeCheckoutProps) => {
-  try {
-    const productWithPrices = await db.product.findMany({
-      where: { id: { in: products.map((product) => product.id) } },
-    });
+  const productWithPrices = await db.product.findMany({
+    where: { id: { in: products.map((product) => product.id) } },
+  });
 
-    if (!stripeKey) throw new Error("Stripe key not found");
+  if (!stripeKey) throw new Error("Stripe key not found");
 
-    const stripe = new Stripe(stripeKey, { apiVersion: "2025-02-24.acacia" });
+  const stripe = new Stripe(stripeKey, { apiVersion: "2025-02-24.acacia" });
 
-    const reqHeaders = await headers();
-    const origin = reqHeaders.get("origin") ?? "";
+  const reqHeaders = await headers();
+  const origin = reqHeaders.get("origin") ?? "";
 
-    const searchParams = new URLSearchParams();
-    searchParams.set("cpf", removeCpfPunctuation(cpf));
-    searchParams.set("consumptionMethod", consumptionMethod);
+  const searchParams = new URLSearchParams();
+  searchParams.set("cpf", removeCpfPunctuation(cpf));
+  searchParams.set("consumptionMethod", consumptionMethod);
 
-    const url = `${origin}/${slug}/orders?${searchParams.toString()}`;
+  const url = `${origin}/${slug}/orders?${searchParams.toString()}`;
 
-    const session = await stripe.checkout.sessions.create({
-      mode: "payment",
-      cancel_url: url,
-      success_url: url,
-      metadata: { orderId },
-      payment_method_types: ["card"],
-      line_items: products.map((product) => ({
-        price_data: {
-          currency: "brl",
-          unit_amount:
-            productWithPrices.find((p) => p.id === product.id)!.price * 100,
-          product_data: { name: product.name, images: [product.imageUrl] },
-        },
-        quantity: product.quantity,
-      })),
-    });
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    cancel_url: url,
+    success_url: url,
+    metadata: { orderId },
+    payment_method_types: ["card"],
+    line_items: products.map((product) => ({
+      price_data: {
+        currency: "brl",
+        unit_amount:
+          productWithPrices.find((p) => p.id === product.id)!.price * 100,
+        product_data: { name: product.name, images: [product.imageUrl] },
+      },
+      quantity: product.quantity,
+    })),
+  });
 
-    return { sessionId: session.id };
-  } catch (error) {
-    console.log("🚀 ~ error:", error);
-  }
+  return { sessionId: session.id };
 };
